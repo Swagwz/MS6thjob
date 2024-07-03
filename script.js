34a21ff931aedfd67bcb4724da612ceb1299ecd4
Swagwz <feelstyle1103@gmail.com>
2024-07-04 07:15:15 +0800

    更新精通2跟共通核心

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const btn = document.querySelector(".btn-submit");
 const form = document.querySelector("form");
-const skillNum = document.querySelector(".skill-num");
-const masteryNum = document.querySelector(".mastery-num");
-const enhancement1Num = document.querySelector(".enhancement-1-num");
-const enhancement2Num = document.querySelector(".enhancement-2-num");
-const enhancement3Num = document.querySelector(".enhancement-3-num");
-const enhancement4Num = document.querySelector(".enhancement-4-num");
-const progressNum = document.querySelector(".progress-num");
+const skillNum = document.querySelector(".skill-progress");
+const commonNum = document.querySelector(".common-progress");
+const mastery1Num = document.querySelector(".mastery-1-progress");
+const mastery2Num = document.querySelector(".mastery-2-progress");
+const enhancement1Num = document.querySelector(".enhancement-1-progress");
+const enhancement2Num = document.querySelector(".enhancement-2-progress");
+const enhancement3Num = document.querySelector(".enhancement-3-progress");
+const enhancement4Num = document.querySelector(".enhancement-4-progress");
+const progressNum = document.querySelector(".total-progress");
+const fragmentNum = document.querySelector(".total-fragment");
+const allInputs = document.querySelectorAll("input");
+
+// 從LocalStorage拿data
+document.getElementById("skill").value = localStorage.getItem("skill");
+document.getElementById("common").value = localStorage.getItem("common");
+document.getElementById("mastery-1").value = localStorage.getItem("mastery1");
+document.getElementById("mastery-2").value = localStorage.getItem("mastery2");
+document.getElementById("enhancement-1").value =
+  localStorage.getItem("enhancement1");
+document.getElementById("enhancement-2").value =
+  localStorage.getItem("enhancement2");
+document.getElementById("enhancement-3").value =
+  localStorage.getItem("enhancement3");
+document.getElementById("enhancement-4").value =
+  localStorage.getItem("enhancement4");
 
 // 技能核心 6轉技能
 const skillCore = [
@@ -168,50 +186,107 @@ const calcErdaFragNum = function (coreArr) {
 const maxSkill = calcErdaFragNum(skillCore);
 const maxMastery = calcErdaFragNum(masteryCore);
 const maxEnhancement = calcErdaFragNum(enhancementCore);
-const total = maxSkill + maxMastery + 4 * maxEnhancement;
+const maxCommon = calcErdaFragNum(commonCore);
+const total = maxSkill + maxCommon + 2 * maxMastery + 4 * maxEnhancement;
 
 btn.addEventListener("click", function () {
   //獲得按下按鈕當下的input value
   const skill = document.getElementById("skill").value;
-  const mastery = document.getElementById("mastery").value;
+  const common = document.getElementById("common").value;
+  const mastery1 = document.getElementById("mastery-1").value;
+  const mastery2 = document.getElementById("mastery-2").value;
   const enhancement1 = document.getElementById("enhancement-1").value;
   const enhancement2 = document.getElementById("enhancement-2").value;
   const enhancement3 = document.getElementById("enhancement-3").value;
   const enhancement4 = document.getElementById("enhancement-4").value;
 
+  // LocalStorage 紀錄數值
+  localStorage.setItem("skill", skill);
+  localStorage.setItem("common", common);
+  localStorage.setItem("mastery1", mastery1);
+  localStorage.setItem("mastery2", mastery2);
+  localStorage.setItem("enhancement1", enhancement1);
+  localStorage.setItem("enhancement2", enhancement2);
+  localStorage.setItem("enhancement3", enhancement3);
+  localStorage.setItem("enhancement4", enhancement4);
+
   // 技能核心進度
-  const skillProgress = calcErdaFragNum(skillCore.slice(0, skill)) / maxSkill;
+  const skillFrag = calcErdaFragNum(skillCore.slice(0, skill));
+  const skillProgress = skillFrag / maxSkill;
+
+  // 共通核心進度
+  const commonFrag = calcErdaFragNum(commonCore.slice(0, common));
+  const comonProgress = commonFrag / maxCommon;
 
   // 精通核心進度
-  const masteryProgress =
-    calcErdaFragNum(masteryCore.slice(0, mastery)) / maxMastery;
+  const mastery1Frag = calcErdaFragNum(masteryCore.slice(0, mastery1));
+  const masteryProgress1 = mastery1Frag / maxMastery;
+
+  const mastery2Frag = calcErdaFragNum(masteryCore.slice(0, mastery2));
+  const masteryProgress2 = mastery2Frag / maxMastery;
 
   // 強化核心進度
-  const enhancement1Progress =
-    calcErdaFragNum(enhancementCore.slice(0, enhancement1)) / maxEnhancement;
-  const enhancement2Progress =
-    calcErdaFragNum(enhancementCore.slice(0, enhancement2)) / maxEnhancement;
-  const enhancement3Progress =
-    calcErdaFragNum(enhancementCore.slice(0, enhancement3)) / maxEnhancement;
-  const enhancement4Progress =
-    calcErdaFragNum(enhancementCore.slice(0, enhancement4)) / maxEnhancement;
+  const enhance1Frag = calcErdaFragNum(enhancementCore.slice(0, enhancement1));
+  const enhancement1Progress = enhance1Frag / maxEnhancement;
+
+  const enhance2Frag = calcErdaFragNum(enhancementCore.slice(0, enhancement2));
+  const enhancement2Progress = enhance2Frag / maxEnhancement;
+
+  const enhance3Frag = calcErdaFragNum(enhancementCore.slice(0, enhancement3));
+  const enhancement3Progress = enhance3Frag / maxEnhancement;
+
+  const enhance4Frag = calcErdaFragNum(enhancementCore.slice(0, enhancement4));
+  const enhancement4Progress = enhance4Frag / maxEnhancement;
 
   // 六轉總進度
   const totalProgress =
     (skillProgress * maxSkill +
-      masteryProgress * maxMastery +
+      comonProgress * maxCommon +
+      masteryProgress1 * maxMastery +
+      masteryProgress2 * maxMastery +
       enhancement1Progress * maxEnhancement +
       enhancement2Progress * maxEnhancement +
       enhancement3Progress * maxEnhancement +
       enhancement4Progress * maxEnhancement) /
     total;
 
+  const totalFragment =
+    skillFrag +
+    commonFrag +
+    mastery1Frag +
+    mastery2Frag +
+    enhance1Frag +
+    enhance2Frag +
+    enhance3Frag +
+    enhance4Frag;
+
   // 改變html上顯示的數值
-  skillNum.textContent = (skillProgress * 100).toFixed(2);
-  masteryNum.textContent = (masteryProgress * 100).toFixed(2);
-  enhancement1Num.textContent = (enhancement1Progress * 100).toFixed(2);
-  enhancement2Num.textContent = (enhancement2Progress * 100).toFixed(2);
-  enhancement3Num.textContent = (enhancement3Progress * 100).toFixed(2);
-  enhancement4Num.textContent = (enhancement4Progress * 100).toFixed(2);
-  progressNum.textContent = (totalProgress * 100).toFixed(2);
+  skillNum.textContent = `進度 : ${(skillProgress * 100).toFixed(2)}%`;
+  commonNum.textContent = `進度 : ${(comonProgress * 100).toFixed(2)}%`;
+  mastery1Num.textContent = `進度 : ${(masteryProgress1 * 100).toFixed(2)}%`;
+  mastery2Num.textContent = `進度 : ${(masteryProgress2 * 100).toFixed(2)}%`;
+  enhancement1Num.textContent = `進度 : ${(enhancement1Progress * 100).toFixed(
+    2
+  )}%`;
+  enhancement2Num.textContent = `進度 : ${(enhancement2Progress * 100).toFixed(
+    2
+  )}%`;
+  enhancement3Num.textContent = `進度 : ${(enhancement3Progress * 100).toFixed(
+    2
+  )}%`;
+  enhancement4Num.textContent = `進度 : ${(enhancement4Progress * 100).toFixed(
+    2
+  )}%`;
+  progressNum.textContent = `目前6轉進度為: ${(totalProgress * 100).toFixed(
+    2
+  )}%`;
+  fragmentNum.textContent = `累積碎片量為: ${totalFragment}個`;
 });
+
+// 限制input max 跟 min
+allInputs.forEach((input) =>
+  input.addEventListener("input", (e) => {
+    if (+e.target.value >= 30) e.target.value = 30;
+    else if (+e.target.value < 0) e.target.value = 0;
+  })
+);
